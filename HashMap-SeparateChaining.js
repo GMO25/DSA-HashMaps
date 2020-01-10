c24435333837b84a59a26c44029408a5e45a6c5d
GabMn <gabrielmanns@live.com>
2020-01-10 17:42:21 +0000

    3rd commit

--- HashMap-SeparateChaining.js
+++ HashMap-SeparateChaining.js
@@ -1,11 +1,115 @@
 
-class Node {
+class _Node {
     constructor(value,next,key){
         this.value = value;
         this.next = next;
        }
 }
-
+class LinkedList {
+    constructor(){
+     this.head = null;
+    }
+   
+    insertBefore(node,item){
+     
+     let nodeAfter = this.find(node) //Boomer
+     let currNode = this.head; // Apollo
+     
+      while(currNode){ // ApolloNode.next == Boomer
+        
+      if(currNode.next == nodeAfter){ // if Apollo.next == Boomer - Yes
+       currNode.next = new _Node(item,nodeAfter) // Apollo.next = newNode 
+       currNode = currNode.next
+       }
+       currNode = currNode.next // node 1,2,3,4,5
+     }
+     
+    }
+    insertAfter(node,item){
+     let nodeBefore = this.find(node) 
+     
+     nodeBefore.next = new _Node(item,nodeBefore.next)
+    
+     let currNode = this.find(item) // hotdog.key // pizza
+     let nodeAfter = this.find(item).next // husker.key  // hotdog
+     while(currNode.key < nodeAfter){
+      currNode = currNode.next
+      }
+   
+    
+    
+   
+   
+    }
+    insertAt(position,item){
+     let currNode = this.head 
+     let count = 0
+     while (currNode){
+       count ++ 
+      currNode = currNode.next
+     
+     if (count == position - 1){
+      currNode.next = new _Node(item,currNode.next)
+      currNode = currNode.next
+     }
+    }
+   
+   }
+   
+   
+   
+    insertFirst(item){
+     this.head = new _Node(item,this.head);
+    }
+    insertLast(item,key){
+     if(this.head === null){
+      this.insertFirst(item,key);
+     } else {
+      let tempNode = this.head;
+      while(tempNode.next !== null){
+       tempNode = tempNode.next;
+      }
+      tempNode.next = new _Node(item,null,key)
+     }
+    }
+    find(item){
+     let currNode = this.head;
+     if(!this.head){
+      return null;
+     }
+     while(currNode.value !== item){
+      if(currNode.next === null){
+       return null;
+      }
+      else {
+       currNode = currNode.next;
+      }
+     }
+     return currNode;
+    }
+    remove(item){
+     if(!this.head){
+      return null;
+     }
+     if (this.head.value === item){
+      this.head = this.head.next;
+      return
+     }
+     let currNode = this.head;
+     let previousNode = this.head;
+     while((currNode !== null) && (currNode.value !== item)){
+      previousNode = currNode;
+      currNode = currNode.next;
+     }
+     if(currNode === null){
+      console.log('Item not found')
+      return;
+     }
+     previousNode.next = currNode.next;
+    }
+   }
+   
+   
 class HashMapSep {
  constructor(initialCapacity=8) {
      this.length = 0;
@@ -15,7 +119,7 @@ class HashMapSep {
      this.MAX_LOAD_RATIO = 0.5;
      this.SIZE_RATIO = 3;
  }
- set(key,value,nextVal){
+ set(key,value){
   const loadRatio = (this.length + this._deleted + 1) / this._capacity;
   if (loadRatio > this.MAX_LOAD_RATIO) {
       this._resize(this._capacity * this.SIZE_RATIO);
@@ -23,30 +127,31 @@ class HashMapSep {
   //Find the slot where this key should be in
   let index = this._findSlot(key);
 
+  const list = new LinkedList
+  
+
   if(!this._hashTable[index]){
-      this.length++; // length increased when key not already present
+      this.length++;
+     
+      console.log(list)
+      this._hashTable[index] = {
+          key,
+          value:value,
+          DELETED:false,
+      }
+  } else {
+      console.log(value)
+      let List = new LinkedList
+      List.insertFirst(value)
+      List.insertLast(this.get(key))
+        this._hashTable[index] = {
+        key,
+        value: List.head,
+        DELETED:false,
+    }
   }
 
-  if(this._hashTable[index]){
-    console.log(index,this.get(key),value)
   
-   // console.log(nextVal)
-   
-    this._hashTable[index] = {
-        key,
-        value: new Node(value,this.get(key)),
-        DELETED : false,    
-       }
-  } 
-      else {
-    this._hashTable[index] = {
-        key,
-        value: new Node(value),
-        DELETED : false,
-        
-       }
-
-}
 
  
 }
@@ -130,6 +235,8 @@ hash.set("Hobbit","Bilbo")
 hash.set("Hobbit","Frodo")
 hash.set("Halfelven","Arwen")
 hash.set("Hobbit","Pippin")
+
+//console.log(hash.get("Ent"))
 console.log(hash)
 console.log(hash.get("Hobbit"))
 
